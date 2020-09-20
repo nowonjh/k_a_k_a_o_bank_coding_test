e3665285b2fbeb62e57fe2f137017d01abb366b4
yuganji <yuganji@localhost>
2020-09-21 02:39:32 +0900

    [ANSWER2] 메뉴 조건 login, logout를 제거

--- answer2/answer2.sql
+++ answer2/answer2.sql
@@ -1,9 +1,9 @@
 SELECT
-    t1.menu_nm                                        AS '메뉴명',
-    t1.lag_menu_nm                                    AS '이전 메뉴명',
+    t1.menu_nm                                      AS '메뉴명',
+    t1.lag_menu_nm                                  AS '이전 메뉴명',
     COUNT(*)                                        AS '접근 건수',
     TRUNCATE(COUNT(*) / SUM(COUNT(*))
-        OVER (PARTITION BY t1.menu_nm) * 100, 2)     AS '비율(%)'
+        OVER (PARTITION BY t1.menu_nm) * 100, 2)    AS '비율(%)'
 FROM (
     SELECT
         log_id,
@@ -12,8 +12,6 @@ FROM (
         OVER(ORDER BY usr_no, log_id) AS lag_menu_nm
     FROM
         MENU_LOG
-    WHERE
-        menu_nm NOT IN ( 'login', 'logout' )
 ) t1
 WHERE
     t1.lag_menu_nm IS NOT NULL
