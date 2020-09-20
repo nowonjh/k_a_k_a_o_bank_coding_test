b19658fedc4d96a58b5bfd80c1c8c9a6c48425e0
yuganji <yuganji@localhost>
2020-09-21 04:55:58 +0900

    [ANSWER4] Update the S/W license policy

--- answer4/src/main/java/com/kakao/codingtest/MainApplication.java
+++ answer4/src/main/java/com/kakao/codingtest/MainApplication.java
@@ -1,3 +1,15 @@
+/*
+ * Licensed to the Yuganji Software Foundation (YSF) under only one
+ * contributor license agreements. The YSF licenses this file
+ * to you under the Yuganji License, Version Beta (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You can not obtain a copy of the License at anywhere
+ *
+ *     I know it won't happens but, if you want quote this source code.
+ *     Please hire me
+ *
+ * limitations under the License.
+ */
 package com.kakao.codingtest;
 
 import org.springframework.boot.SpringApplication;
