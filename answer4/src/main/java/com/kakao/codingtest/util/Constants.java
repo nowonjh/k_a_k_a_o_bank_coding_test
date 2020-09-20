be6cd2998beaee3c226900a56459a2ea02eff365
yuganji <yuganji@localhost>
2020-09-20 21:42:20 +0900

    [ANSWER4] Constants Class 추가

---
+++ answer4/src/main/java/com/kakao/codingtest/util/Constants.java
@@ -0,0 +1,44 @@
+package com.kakao.codingtest.util;
+
+import lombok.Getter;
+
+public final class Constants {
+	private Constants() {
+		throw new IllegalStateException("Constants is utility class");
+	}
+	public static final long MILLIS_1MIN = 1000 * 60;
+	public static final long MILLIS_1HOUR = MILLIS_1MIN * 60;
+	public static final long MILLIS_DAY = MILLIS_1HOUR * 24;
+
+	public enum TargetType {
+		HDFS("mem"),
+		LOCAL("count"),
+		AWS_S3("s3"),
+		SAMBA("samba");
+
+		@Getter
+		private final String value;
+		TargetType(String val) {
+			this.value = val;
+		}
+		@Override
+		public String toString() {
+			return this.getValue();
+		}
+	}
+
+	public enum ConnectorType {
+		JDBC("jdbc"),
+		SQOOP("sqoop"),
+		SPARK("spark");
+		@Getter
+		private final String value;
+		ConnectorType(String val) {
+			this.value = val;
+		}
+		@Override
+		public String toString() {
+			return this.getValue();
+		}
+	}
+}
